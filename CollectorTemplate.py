b17d2c0f487abe028e0775de1fd1362f0395dfcd
John Hernandez Valerio <45037500+Johnmhv19@users.noreply.github.com>
2025-04-21 08:41:35 +0800

    Add files via upload

---
+++ CollectorTemplate.py
@@ -0,0 +1,37 @@
+#Import the libraries
+import pgzrun
+from random import randint
+from time import time
+
+# Create variables and elements
+player = Actor('player')
+coin = Actor('coin')
+
+#Resize the screen using WIDTH and HEIGHT= #
+
+#For all the functions below, remove Pass 
+#Whenever you write your own code.
+
+#Function to draw on the screen
+def draw():
+    # Draw actors
+    pass
+
+
+#Function to update the position when 
+#pressing the moving keys or when coin
+#is touched.
+def update():
+    # Move player with keys
+    # Check for collisions
+    pass
+
+#Function for the restart game funcitonality
+def on_key_down(key):
+    # Handle key input
+    pass
+
+#reset time
+def reset_time():
+    pass
+
